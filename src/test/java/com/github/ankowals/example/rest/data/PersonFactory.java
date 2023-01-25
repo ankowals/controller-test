74e7646646238ce6cb3e0876868763d9a6f8626d
Andrzej Kowalski <31848642+ankowals@users.noreply.github.com>
2023-01-25 20:53:33 +0100

    read objects from resource file

--- src/test/java/com/github/ankowals/example/rest/data/PersonFactory.java
+++ src/test/java/com/github/ankowals/example/rest/data/PersonFactory.java
@@ -1,12 +1,17 @@
 package com.github.ankowals.example.rest.data;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.ankowals.example.rest.client.JacksonMapperFactory;
 import com.github.ankowals.example.rest.domain.Person;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class PersonFactory {
 
     private final RandomizationStrategy<Person> randomizationStrategy;
+    private final ObjectMapper objectMapper = JacksonMapperFactory.create();
 
     public PersonFactory(RandomizationStrategy<Person> randomizationStrategy) {
         this.randomizationStrategy = randomizationStrategy;
@@ -25,5 +30,21 @@ public class PersonFactory {
         customizer.accept(person);
 
         return person;
+    }
+
+    public Person personFrom(String filename) throws IOException {
+        Person person = readResourceFileAs(filename, Person.class);
+        return randomizationStrategy.randomize(person);
+    }
+
+    public List<Person> personsFrom(String filename) throws IOException {
+        List<Person> persons = List.of(readResourceFileAs(filename, Person[].class));
+        persons.forEach(randomizationStrategy::randomize);
+
+        return persons;
+    }
+
+    private <T> T readResourceFileAs(String filename, Class<T> clazz) throws IOException {
+        return objectMapper.readValue(this.getClass().getResourceAsStream(filename), clazz);
     }
 }
